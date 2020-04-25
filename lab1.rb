08fb4d2b97553fdad2ea17864a9c94f8ccf2eb3e
logainhassan <logeenhassanmostafa96@gmail.com>
2020-04-25 16:36:54 -0700

    get stats

--- lab1.rb
+++ lab1.rb
@@ -3,21 +3,26 @@ class ComplexNumber
 
   attr_reader :real, :imaginary
 
+  @@state = {"+{cn1}" => 0,"*{cn1}" => 0,"bulk_add" => 0,"bulk_multiply" => 0}
+
   def initialize(real,imaginary)
     @real = real
     @imaginary = imaginary
   end
 
   def +(cn1)
+    @@state["+{cn1}"]+=1
     ComplexNumber.new(self.real+cn1.real, self.imaginary+cn1.imaginary)
   end
 
   def *(cn1)
+    @@state["*{cn1}"]+=1
     ComplexNumber.new(self.real * cn1.real - self.imaginary * cn1.imaginary, self.imaginary * cn1.real + self.real * cn1.imaginary)
   end
 
   def bulk_add(cns)
-   sum = ComplexNumber.new(0,0)
+    @@state["bulk_add"]+=1
+    sum = ComplexNumber.new(0,0)
     if cns.length > 0 then
       cns.each do |item|
         sum += item
@@ -27,13 +32,18 @@ class ComplexNumber
   end
 
   def bulk_multiply(cns)
-   result = cns[0]
+    @@state["bulk_multiply"]+=1
+    result = cns[0]
     if cns.length > 0 then
       (1...cns.length).each do |item|
         result *= cns[item]
       end
     end
     return result
+  end
+
+  def get_stats()
+    @@state
   end
 
 end
@@ -51,8 +61,11 @@ puts "Complex + form : #{obj.real } + #{obj.imaginary}i\n\n"
 obj = obj1 * obj2
 puts "Complex * form : #{obj.real } + #{obj.imaginary}i\n\n"
 
-obj = [obj1,obj2];
+obj = [obj1,obj2,obj3];
 
 puts "Complex array + form : #{obj1.bulk_add(obj).real } + #{obj1.bulk_add(obj).imaginary}i\n\n"
 
 puts "Complex array * form : #{obj2.bulk_multiply(obj).real } + #{obj2.bulk_multiply(obj).imaginary}i\n\n"
+
+state = obj1.get_stats()
+puts "number of operations #{state} \n\n"
